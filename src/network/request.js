f5ab93e307300bce69644c19ca48a9947e19938c
sunkebkn8 <sunkebkn8@live.com>
2020-07-14 17:03:28 +0800

    sss

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:8000/api/n3',
+    baseURL: '',
     timeout: 10000
   })
   instance.interceptors.request.use(config => {
